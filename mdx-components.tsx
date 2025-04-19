3a2787c6ece8f7e4eb0a2bb0be094be0ee21e40a
blithersoup <gradyarnold11@gmail.com>
2025-04-18 20:16:21 -0500

    comment

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -56,43 +56,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     Img,
     Row,
     
-    // img: ({ 
-    //   src, alt, caption, priority
-    // }: { 
-    //   src: string, alt: string, caption?: string, priority?: boolean 
-    // }) => (
-    //   <div className="w-full sm:w-[calc(50%-12px)] md:w-[calc(33.333%-16px)] lg:w-[calc(25%-18px)] mb-6">
-    //   <div className="aspect-square relative overflow-hidden rounded-lg shadow-lg border border-gray-200">
-    //     <Image
-    //       src={src}
-    //       caption={alt}
-    //       fill
-    //       priority={priority}
-    //       sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-    //       className="object-cover"
-    //     />
-    //   </div>
-    // </div>
-    // ),
-
-    // // Responsive images
-    // img: (props) => (
-    //   <div className="my-10">
-    //     <Image
-    //       className="rounded-md mx-auto"
-    //       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 760px"
-    //       style={{ width: '100%', height: 'auto' }}
-    //       {...(props as ImageProps)}
-    //       caption={props.alt || ""}
-    //     />
-    //     {props.alt && (
-    //       <figcaption className="mt-3 text-center text-sm text-neutral-500 dark:text-neutral-400">
-    //         {props.alt}
-    //       </figcaption>
-    //     )}
-    //   </div>
-    // ),
-    
     ...components,
   }
 }
